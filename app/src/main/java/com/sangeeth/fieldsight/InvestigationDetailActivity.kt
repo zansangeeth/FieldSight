bfd2149fc7906d7533ef8c3988ecdf086820f34b
Sangeeth Amirthanathan <zansangeeth17@gmail.com>
2026-02-13 18:01:09 +0000

    implemented back button functionality for investigation details screen

--- app/src/main/java/com/sangeeth/fieldsight/InvestigationDetailActivity.kt
+++ app/src/main/java/com/sangeeth/fieldsight/InvestigationDetailActivity.kt
@@ -14,47 +14,57 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.sangeeth.fieldsight.ui.theme.FieldSightTheme
 
 class InvestigationDetailActivity : AppCompatActivity() {
-
-
-    @OptIn(ExperimentalMaterial3Api::class)
-    override fun onCreate(savedInstanceState: Bundle?) {
+ override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
             val navController = rememberNavController()
-
             FieldSightTheme {
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = {
-                                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                                    Text("Field Sight")
-                                }
-                            },
-                            navigationIcon = {
-                                IconButton(onClick = { navController.popBackStack() }){
-                                    Icon(
-                                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                                        contentDescription = "Go Back"
-                                    )
-                                }
-                            }
+                NavHost(navController, startDestination = "investigation_detail"){
+                    composable("investigation_detail") {
+                        TopBarNavigationOnBack(
+                            navigationBack = { finish() }
                         )
-                    },
-                    content = { innerPadding->
-                        Text("Investigation Detail", modifier = Modifier.padding(innerPadding))
                     }
-                )
+                }
             }
         }
     }
-
 }
 
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun TopBarNavigationOnBack(
+    navigationBack: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Text("Field Sight")
+                    }
+                },
+                navigationIcon = {
+                    IconButton(onClick = navigationBack ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Go Back"
+                        )
+                    }
+                }
+            )
+        },
+        content = { innerPadding->
+            Text("Investigation Detail", modifier = Modifier.padding(innerPadding))
+        }
+    )
+}
